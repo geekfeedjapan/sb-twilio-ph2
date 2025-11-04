f848d16ba043a3f07ce341cc11d25eeb5e70706f
Tsuyoshi-Yoshida <76932607+Tsuyoshi-Yoshida@users.noreply.github.com>
2025-11-04 22:28:55 +0900

    削除

--- airplane/QR_registration.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('QRコード事前通知登録ページが読み込まれました');
-});
--- airplane/demo_settings.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('デモ設定ページが読み込まれました');
-});
